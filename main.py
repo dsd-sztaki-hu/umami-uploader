c90be38ddec744dccad96f0778e71e31df74940f
Akos Kukucska <a0sf3c@inf.elte.hu>
2026-07-07 12:35:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import csv
+import json
+
+from datetime import datetime
+
+with open("config.json") as configfile:
+    config = json.load(configfile)
+
+website_ids = config["website_ids"]
+base = config["base"]
+
+with open(config["logfile"]) as f:
+    reader = csv.DictReader(f,["website", "userAgent", "timestamp", "ip"], delimiter='\t')
+    for line in reader:
+        line["hostname"] = line["website"][8:-1]
+        line["website"] = website_ids[line["website"]]
+        line["timestamp"] = int(datetime.fromisoformat(line["timestamp"]).timestamp())
+        line["ip"] = line["ip"].split(":")[0]
+        useragent = line["userAgent"]
+        line.pop("userAgent")
+         
+        print(line)
+
+        result = requests.post(base + "/api/send/", 
+                      headers={'Content-Type': 'application/json',
+                                'User-Agent':useragent},
+                      json={"type": "event", "payload": line})
+        print(result.content)
